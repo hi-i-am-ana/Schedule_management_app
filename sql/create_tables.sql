1caecc937b150ad9f319184ba75b9b2522cecac1
Anastasia Chen <anastasia@Anastasias-MacBook-Air.local>
2021-02-22 10:59:12 +1100

    Add user_id foreign key and end_time check to schedule table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,19 +1,23 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id SERIAL PRIMARY KEY,
+    user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     lastname VARCHAR(50) NOT NULL,
     firstname VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
     password CHAR(60) NOT NULL
 );
 
 DROP TABLE IF EXISTS schedules;
 
 CREATE TABLE IF NOT EXISTS schedules (
-    schedule_id SERIAL PRIMARY KEY,
+    schedule_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id SMALLINT NOT NULL,
     day SMALLINT NOT NULL CHECK (day >= 1 AND day <= 7),
     start_time timestamptz,
-    end_time timestamptz
+    end_time timestamptz CHECK (end_time > start_time),
+
+    FOREIGN KEY(user_id) 
+      REFERENCES users(user_id)
+      ON DELETE CASCADE
 );
